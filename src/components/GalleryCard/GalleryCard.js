d3b5eefad58ae1c8783d24b1d4cf46d3eb6ef2f6
njmbb8 <nmcgr001@gmail.com>
2022-01-18 23:45:10 -0500

    fixed url in gallery card

--- src/components/GalleryCard/GalleryCard.js
+++ src/components/GalleryCard/GalleryCard.js
@@ -4,13 +4,14 @@ import { Card, Col } from "react-bootstrap";
 function GalleryCard({art}){
 
     const [overlay, setOverlay] = useState(false)
+    const {REACT_APP_BACKEND_URL} = process.env
 
     return(
         <>
             <Col>
                 <Card className="text-white">
                     <Card.Img 
-                        src={`http://localhost:4000/${art.photo}`}
+                        src={`${REACT_APP_BACKEND_URL }${art.photo}`}
                         onMouseEnter={()=>setOverlay(!overlay)} 
                     />
                     {!overlay ? <Card.Footer className="text-black">{art.title}</Card.Footer> : null}
