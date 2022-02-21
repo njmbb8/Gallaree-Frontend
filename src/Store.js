79ad1c9338a31ae2c0dffa1317144d6293d6044b
njmbb8 <nmcgr001@gmail.com>
2022-02-21 10:21:32 -0500

    Added artsReduce to store configuration

--- src/Store.js
+++ src/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/User"
+import artsReducer from "./slices/Arts"
 
 const store = configureStore({
     reducer: {
+        arts: artsReducer,
         user: userReducer
     }
 })
