ad792cd296e736ff85b99b97e44d8d0f30c0723e
njmbb8 <nmcgr001@gmail.com>
2022-02-21 20:27:44 -0500

    Redux state is now persisting!

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    persistReducer,
+    persistStore,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+  } from 'redux-persist';
+import storage from "redux-persist/lib/storage";
 import userReducer from "./slices/User"
 import artsReducer from "./slices/Arts"
 
-const store = configureStore({
-    reducer: {
-        arts: artsReducer,
-        user: userReducer
-    }
+const rootReducer = combineReducers({
+    arts: artsReducer,
+    user: userReducer
 })
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: {
+            ignoreActions: [ FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER ]
+        }
+    })
+})
+
+export const persistor = persistStore(store)
 export default store
