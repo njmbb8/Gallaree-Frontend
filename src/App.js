877741d4cbf3275a9d6a8756c49d8c28fd74f0ba
njmbb8 <nmcgr001@gmail.com>
2022-03-21 11:59:19 -0400

    set up app for stripe

--- src/App.js
+++ src/App.js
@@ -7,28 +7,31 @@ import Gallery from './components/Gallery/Gallery';
 import ArtForm from './components/ArtForm/ArtForm';
 import { Container } from 'react-bootstrap';
 import ArtDisplay from './components/ArtDisplay/ArtDisplay';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { authenticate } from './slices/User';
 import { populate } from './slices/Arts'
 import { TailSpin } from 'react-loader-spinner';
 import PasswordReset from './components/PasswordReset/PasswordReset';
 import Confirmation from './components/Confirmation/Confirmation';
 import { updateOrderItems } from './slices/Order';
+import { setClientSecret } from './slices/ClientSecret';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 function App() {
   const [ statuses, setStatuses ] = useState([])
-  // const [ showRegister, setShowRegister ] = useState(false)
-  // const [showSignIn, setShowSignIn] = useState(false)
   const dispatch = useDispatch()
-  const { REACT_APP_BACKEND_URL } = process.env
+  const { REACT_APP_BACKEND_URL, REACT_APP_STRIPE_PUBLISHABLE_KEY } = process.env
   const [ ready, setReady ] = useState(false)
-
+  const clientSecret = useSelector(state => state.clientSecret)
+  const stripePromise = loadStripe(REACT_APP_STRIPE_PUBLISHABLE_KEY) 
+  
   useEffect(() => {
     fetch(`${REACT_APP_BACKEND_URL}/statuses`)
     .then((data) => data.json())
     .then((ret)=>setStatuses(ret))
   }, [REACT_APP_BACKEND_URL])
-
+  
   useEffect(() => {
     fetch(`${REACT_APP_BACKEND_URL}/arts`)
     .then((data) => data.json())
@@ -37,7 +40,7 @@ function App() {
       setReady(true)
     })
   }, [REACT_APP_BACKEND_URL, dispatch])
-
+  
   useEffect(()=>{
     if(!!document.cookie.split('; ').find(row => row.startsWith('user_id='))){
       fetch(`${REACT_APP_BACKEND_URL}/me`,{
@@ -47,25 +50,37 @@ function App() {
       .then((ret) => {
         dispatch(authenticate(ret))
         fetch(`${REACT_APP_BACKEND_URL}/order/${ret.active_order.id}`, {
-            method: 'GET',
-            credentials: 'include'
+          method: 'GET',
+          credentials: 'include'
         })
         .then((orderData) => orderData.json())
         .then((orderJSON) =>dispatch(updateOrderItems(orderJSON)))
       })
     }
   }, [REACT_APP_BACKEND_URL, dispatch])
+  
+  useEffect(() => {
+    fetch(`${REACT_APP_BACKEND_URL}/payment_intents`, {
+      method: 'POST',
+      credentials: 'include'
+    })
+    .then((data) => data.json())
+    .then((ret) => dispatch(setClientSecret(ret)))
+  }, [REACT_APP_BACKEND_URL])
 
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+  
   return (
     <div>
       {ready ? 
       <>
-        <Navbar 
-          // showRegister = {showRegister}
-          // setShowRegister={setShowRegister}
-          // showSignIn={showSignIn}
-          // setShowSignIn={setShowSignIn}
-        />
+        <Navbar/>
         <Container className="content">
           <Routes>
             <Route 
@@ -93,6 +108,12 @@ function App() {
             <Route
               path={'/confirm/:token'}
               element={<Confirmation />}
+            />
+            <Route
+              path={'/checkout'}
+              element = {
+                <Elements options={options} stripe={stripePromise}></Elements>
+              }
             />
           </Routes>
         </Container>
