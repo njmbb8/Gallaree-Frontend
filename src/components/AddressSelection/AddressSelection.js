3aa0dbbe89e8257f0ca6850ec2400902899d0657
njmbb8 <nmcgr001@gmail.com>
2022-04-06 21:29:46 -0400

    overhaul address selection form

--- src/components/AddressSelection/AddressSelection.js
+++ src/components/AddressSelection/AddressSelection.js
@@ -1,132 +1,131 @@
 import React, {useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { FormLabel, FormSelect, Button, Row, ButtonGroup } from "react-bootstrap";
+import { FormLabel, FormSelect, Button, Row, ButtonGroup, Form } from "react-bootstrap";
 import { authenticate } from "../../slices/User";
 import AddressForm from "../AddressForm/AddressForm";
+import { updateOrderItems } from "../../slices/Order";
 
 function AddressSelection({shipping,setShipping}){
-    const dispatch = useDispatch()
-    const { REACT_APP_BACKEND_URL } = process.env
     const user = useSelector(state => state.user)
-    const [showEdit, setShowEdit] = useState(false)
-    const [addressMode, setAddressMode] = useState('')
-    const [editShipping, setEditShipping] = useState(0)
-    const [addrToEdit, setAddrToEdit] = useState(0)
-    const blank = {
-        address_line1: '',
-        address_line2: '',
-        city: '',
-        state: 0,
-        postal_code: '',
-        country: '',
+    const [showAddressForm, setShowAddressForm] = useState(false)
+    const [addressFormMode, setAddressFormMode] = useState("")
+    const [addressToEdit, setAddressToEdit] = useState({})
+    const {REACT_APP_BACKEND_URL} = process.env
+    const dispatch = useDispatch()
+    const addressOptions = user.addresses.map((address) =>{
+        if(address.shipping){
+            setShipping(address)
+        }
+        return (
+            <option selected={address.shipping} value={address.id}>{address.address_line1}</option>
+        )
+    })
+    const blankAddress={
+        address_line1: "",
+        address_line2: "",
+        city: "",
+        state: "",
+        postal_code: "",
+        country: "",
         shipping: false
     }
 
-    const addressOptions = user.addresses.map((address, index) => {
-        if(address.shipping && shipping === null){
-            setShipping(index)
-        }
-        return <option key={address.id} value={index}>{`${address.address_line1}`}</option>
-    })
+    function updateAddress(){
+        addressData = new FormData()
+        addressData.append('address_line1', shipping.address_line1)
+        addressData.append('address_line2', shipping.address_line2)
+        addressData.append('city', shipping.city)
+        addressData.append('state', shipping.state)
+        addressData.append('postal_code', shipping.zip)
+        addressData.append('country', shipping.country)
+        addressData.append('shipping', shipping.shipping)
 
-    function updateAddresses(address){
-        const addrData = new FormData()
-        addrData.append('address_line1', address.address_line1)
-        addrData.append('address_line2', address.address_line2)
-        addrData.append('city', address.city)
-        addrData.append('state', address.state)
-        addrData.append('country', address.country)
-        addrData.append('postal_code', address.postal_code)
-        addrData.append('shipping', addrData.shipping)
-        fetch(`${REACT_APP_BACKEND_URL}/addresses/${address.id}`, {
-            method: "PATCH",
-            credentials: "include",
+        fetch(`${REACT_APP_BACKEND_URL}/address/${shipping.id}`,{
+            method: 'PATCH',
             headers: {
-                "Accept": "application/json"
+                Accepts: 'application/json'
             },
-            body: addrData
+            credentials: 'include',
+            body:addressData
         })
-        .then((data)=> data.json())
-        .then((ret)=>{    
-            console.log(ret)
-            dispatch(authenticate({...user, addresses: [...user.addresses, ret]
-        }))
+        .then((data) => data.json)
+        .then((ret)=>{
+            const addresses = user.addresses.map((address) => {
+                if(address.id === ret.id){
+                    return ret
+                }
+                else{
+                    return address
+                }
+            })
+            dispatch(authenticate({...user, addresses: addresses}))
         })
     }
 
-    function deleteAddress(address){
-        fetch(`${REACT_APP_BACKEND_URL}/addresses/${address.id}`, {
-            method: "DELETE",
-            credentials: "include",
+    function setShippingForOrder(e){
+        orderData = new FormData()
+        orderData.append('shipping', e.target.value)
+        fetch(`${REACT_APP_BACKEND_URL}/orders/${user.active_order.id}`, {
+            method: 'PATCH',
+            credentials: 'include',
+            body: orderData
+        })
+        .then((data) => data.json())
+        .then((ret) => {
+            dispatch(updateOrderItems(ret))
+            dispatch(authenticate({...user, active_order: ret}))
+        })
+    }
+
+    function removeAddress(){
+        fetch(`${REACT_APP_BACKEND_URL}/addresses/${shipping.id}`, {
+            method: 'DELETE',
+            credentials: 'include',
         })
         .then(()=>{
-            dispatch(authenticate({...user, addresses: user.addresses.filter((addr)=>{
-                return address.id !== addr.id
-            })}))
+            addresses = user.addresses.filter((address) => address.id !== shipping.id)
+            dispatch(authenticate({...user, addresses: addresses}))
         })
     }
 
-    function deleteShipping(e){
-        e.preventDefault()
-        const removeAddress = user.addresses.find((address) => address.id === user.addresses[shipping].id)
-        deleteAddress(removeAddress)
-    }
-
-    function defaultShipping(e){
-        e.preventDefault()
-        const activeAddress = user.addresses.find((address) => address.id === user.addresses[shipping].id)
-        updateAddresses({...activeAddress, shipping: true})
-    }
-
-    function expandEdit(e){
-        e.preventDefault()
-        setAddressMode('edit')
-        setEditShipping(e.target.id === 'shippingEdit'? 1 : 2)
-        if(editShipping === 1){
-            setAddrToEdit(user.addresses[shipping])
+    function displayAddressForm(e){
+        setAddressFormMode(e.target.id)
+        if(addressFormMode === "new"){
+            setAddressToEdit(blankAddress)
         }
-        setShowEdit(!showEdit)
-    }
+        else if(addressFormMode === "update"){
+            setAddressToEdit(shipping)
+        }
+        setShowEdit(true)
 
-    function expandNew(e){
-        e.preventDefault()
-        setAddrToEdit(blank)
-        setAddressMode('new')
-        setEditShipping(0)
-        setShowEdit(!showEdit)
     }
 
     return(
         <>
-            <FormLabel htmlFor="shipping">Shipping Address:</FormLabel>
-            <Row>
-                <FormSelect id="shipping" defaultValue={shipping} onChange={(e)=>setShipping(e.target.value)}>
-                    {addressOptions}
-                </FormSelect>
-            </Row>
-            <Row>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Shipping Address:</Form.Label>
+                    <Form.Select onChange={setShippingForOrder}>{addressOptions}</Form.Select>
+                </Form.Group>
                 <ButtonGroup>
-                    <Button id="shippingEdit" onClick={expandEdit} variant="primary">Edit</Button>
-                    <Button onClick={deleteShipping} variant="danger">Remove</Button>
-                    <Button onClick={defaultShipping} variant="primary">Make Default</Button>
+                    <Button id="edit" onClick={displayAddressForm}>Edit Address</Button>
+                    <Button variant="danger" onClick={removeAddress}>Delete Address</Button>
+                    {shipping.shipping ? null : <Button>Make Default</Button>}
                 </ButtonGroup>
-            </Row>
-            <Row>
-                <Button onClick={expandNew} variant="primary">New Address</Button>
-            </Row>
-            {
-                showEdit && addrToEdit?
-                    <AddressForm 
-                        mode={addressMode}
-                        address={addrToEdit}
-                        updateAddress={updateAddresses}
-                        showEdit={showEdit}
-                        setShowEdit={setShowEdit}
-                    />
-                :
-                null
-            }
+                <Button id="new" onClick={displayAddressForm}>New Address</Button>
+            </Form>
+            {showAddressForm ? 
+                <AddressForm 
+                    mode={addressFormMode}
+                    address={addressToEdit}
+                    updateAddress={updateAddress}
+                    showEdit={showAddressForm}
+                    setShowEdit={setShowAddressForm}
+                />
+            : 
+                null}
         </>
+
     )
 }
 
