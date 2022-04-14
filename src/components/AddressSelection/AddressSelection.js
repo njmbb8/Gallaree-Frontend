4bea2702ac9db3e27ef9007495357c05fcd4afc8
njmbb8 <nmcgr001@gmail.com>
2022-04-14 16:57:47 -0400

    remove unused variables

--- src/components/AddressSelection/AddressSelection.js
+++ src/components/AddressSelection/AddressSelection.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { FormLabel, FormSelect, Button, Row, ButtonGroup, Form } from "react-bootstrap";
+import { Button, ButtonGroup, Form } from "react-bootstrap";
 import { authenticate } from "../../slices/User";
 import AddressForm from "../AddressForm/AddressForm";
 import { updateOrderItems } from "../../slices/Order";
