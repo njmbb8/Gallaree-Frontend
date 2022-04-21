c545050b2e7760ee5f306fdbaeaeea671e7d2fcd
njmbb8 <nmcgr001@gmail.com>
2022-04-21 10:13:35 -0400

    update ErrorModal to use redux

--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { setError } from '../../slices/Error'
 
-function ErrorModal({error, show, setShow}){
+function ErrorModal(){
+    const error = useSelector(state => state.error)
+    const dispatch = useDispatch()
     return(
-        <Modal show={show} onHide={() => setShow(false)}>
+        <Modal show={error.length > 0} onHide={dispatch(setError(''))}>
             <Modal.Header closeButton>
                 <Modal.Title>Error</Modal.Title>
             </Modal.Header>
             <Modal.Body>{error.error}</Modal.Body>
             <Modal.Footer>
-                <Button variant="primary" onClick={() => setShow(false)}>
+                <Button variant="primary" onClick={dispatch(setError(''))}>
                     Ok
                 </Button>
             </Modal.Footer>
