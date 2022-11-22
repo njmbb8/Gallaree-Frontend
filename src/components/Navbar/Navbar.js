b6e0c50209ccd279e0bd8d97e639533be0f16b01
njmbb8 <nmcgr001@gmail.com>
2022-11-22 17:45:02 -0500

    update navbar to work without order store

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -85,7 +85,7 @@ function Navigation(){
                                     >
                                             {user.stripe_info.name}
                                     </Navbar.Text>
-                                    {!user.admin ? <Navbar.Text onMouseOver={(e)=> e.target.style.cursor = 'pointer'} onClick={expandOrder}>({Object.entries(order).length > 0 ? order.items.length : null})</Navbar.Text> : null}
+                                    {!user.admin ? <Navbar.Text onMouseOver={(e)=> e.target.style.cursor = 'pointer'} onClick={expandOrder}>({user.active_order.items.length})</Navbar.Text> : null}
                                     <Nav.Link href={'/#'} onClick={handleSignOut} >Sign Out</Nav.Link>
                                 </>
                                 :
